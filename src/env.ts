4354516c20e2ce3f1699b6169c4001be68a01193
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2023-08-02 09:12:43 +0530

    Try to fix missing env var errors in Deno Deploy

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,7 @@
-import "https://deno.land/std@0.196.0/dotenv/load.ts";
+import { load } from "https://deno.land/std@0.196.0/dotenv/mod.ts";
 import { cleanEnv, str } from "../deps.ts";
+
+await load({ export: true });
 
 const specs = {
   BOT_TOKEN: str(),
